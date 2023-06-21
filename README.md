1482c6fbc53eaf5574d7a4c1a25614cd0de97b5f
Jaeseob Hwang <jetsbee@gmail.com>
2023-06-21 17:51:03 +0900

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Inspiring Bookmark
+## Environment
+- Node.js
+
+## Scripts
+```bash
+# Install deps
+npm install
+
+# Run dev server
+npm run dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p>Hello, Inspiring bookmark!</p>
+      </div>
+    </main>
+  );
+}
