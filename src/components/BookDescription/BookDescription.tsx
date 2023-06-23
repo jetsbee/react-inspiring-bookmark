bd7f162d21dfb9d4abc4af1c41f63c93e0667858
Jaeseob Hwang <jetsbee@gmail.com>
2023-06-23 18:51:01 +0900

    Add ellipsis

--- src/components/BookDescription/BookDescription.tsx
+++ src/components/BookDescription/BookDescription.tsx
@@ -6,7 +6,9 @@ const BookDescription = ({ children: text }: Props) => {
 
   return (
     <div>
-      <p>{showMore ? text : `${text.substring(0, MIN_TEXT_LENGTH_LIMIT)}`}</p>
+      <p>
+        {showMore ? text : `${text.substring(0, MIN_TEXT_LENGTH_LIMIT)}...`}
+      </p>
       {text.length > MIN_TEXT_LENGTH_LIMIT && (
         <button onClick={onToggle}>
           {showMore ? "Show less" : "Show more"}
