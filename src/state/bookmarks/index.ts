4ab6925ea6c80c31f01f9f6087c17ce299050958
Jaeseob Hwang <jetsbee@gmail.com>
2023-06-26 23:49:07 +0900

    Apply persistency by using local storage

--- src/state/bookmarks/index.ts
+++ src/state/bookmarks/index.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface BookmarksState {
   bookmarks: {
@@ -6,29 +7,34 @@ interface BookmarksState {
   };
 }
 
-interface BookmarksAction {
+interface BookmarksActions {
   addBookmark: (isbn: string) => void;
   removeBookmark: (isbn: string) => void;
 }
 
-const useBookmarksStore = create<BookmarksState & { actions: BookmarksAction }>(
-  (set) => ({
-    bookmarks: {},
-    actions: {
-      addBookmark: (isbn) =>
-        set(({ bookmarks }) => ({
-          bookmarks: { ...bookmarks, [isbn]: true },
-        })), // End of addBookmark()
-      removeBookmark: (isbn) =>
-        set(({ bookmarks }) => {
-          const { [isbn]: removedItem, ...rest } = bookmarks;
-          return { bookmarks: rest };
-        }), // End of removeBookmark()
-    },
-  })
+const useBookmarksStore = create<
+  BookmarksState & { actions: () => BookmarksActions }
+>()(
+  persist(
+    (set) => ({
+      bookmarks: {},
+      actions: () => ({
+        addBookmark: (isbn) =>
+          set(({ bookmarks }) => ({
+            bookmarks: { ...bookmarks, [isbn]: true },
+          })), // End of addBookmark()
+        removeBookmark: (isbn) =>
+          set(({ bookmarks }) => {
+            const { [isbn]: removedItem, ...rest } = bookmarks;
+            return { bookmarks: rest };
+          }), // End of removeBookmark()
+      }),
+    }),
+    { name: "bookmarks-storage" }
+  )
 );
 
-const useBookmarksActions = () => useBookmarksStore((state) => state.actions);
+const useBookmarksActions = () => useBookmarksStore((state) => state.actions());
 
 const useBookmarks = () => useBookmarksStore(({ bookmarks }) => bookmarks);
 
