7c682cc9430260865f7647bd6c5649fd048ce0cd
Jaeseob Hwang <jetsbee@gmail.com>
2023-06-26 20:24:46 +0900

    Add bookmark placeholder

--- src/components/BookmarkStar/BookmarkStar.tsx
+++ src/components/BookmarkStar/BookmarkStar.tsx
@@ -1,14 +1,15 @@
 import { useIsClient } from "@/hooks/isCilent";
 import BookmarkStarView from "./BookmarkStarView/BookmarkStarView";
 import { Props } from "./BookmarkStar.type";
+import { StarLine } from "./BookmarkStarView/BookmarkStarView.styled";
 
 const BookmarkStar = ({ isbn }: Props) => {
   const isClient = useIsClient();
 
-  return (
-    isClient && (
-      <BookmarkStarView filled={Boolean(Math.floor(Math.random() * 2))} />
-    )
+  return isClient ? (
+    <BookmarkStarView filled={Boolean(Math.floor(Math.random() * 2))} />
+  ) : (
+    <StarLine />
   );
 };
 
