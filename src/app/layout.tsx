38e34da5a135bb6c1eb056c882bcb03c10376616
Jaeseob Hwang <jetsbee@gmail.com>
2023-07-02 18:38:20 +0900

    Fix minor
    ref. https://tanstack.com/query/latest/docs/react/guides/ssr#using-the-app-directory-in-nextjs-13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Providers>
-        <body className={inter.className}>
+      <body className={inter.className}>
+        <Providers>
           <GlobalHeader />
           {children}
-        </body>
-      </Providers>
+        </Providers>
+      </body>
     </html>
   );
 }
