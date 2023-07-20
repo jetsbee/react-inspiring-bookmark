ab3f05ef27eb3b2c3c6caa66236e0d9720f01cc9
Jaeseob Hwang <jetsbee@gmail.com>
2023-07-20 17:59:46 +0900

    Fix: bugs not working on SearchPage docs

--- src/app/search/page.stories.tsx
+++ src/app/search/page.stories.tsx
@@ -4,28 +4,14 @@ import { rest } from "msw";
 import SearchPage from "./page";
 import { searchedBooksDB } from "./page.stories.data";
 
+const urlPathForBooksByKeywordsAndStartIndex = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/books/by-keywords/:keywords/:startIndex`;
+
 const meta: Meta<typeof SearchPage> = {
   title: "SearchPage",
   component: SearchPage,
   tags: ["autodocs"],
   decorators: RootPageMeta.decorators,
-};
-
-export default meta;
-type Story = StoryObj<typeof SearchPage>;
-
-const urlPathForBooksByKeywordsAndStartIndex = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/books/by-keywords/:keywords/:startIndex`;
-
-export const Default: Story = {
   parameters: {
-    nextjs: {
-      appDirectory: true,
-      navigation: {
-        query: {
-          keywords: "santa+claus",
-        },
-      },
-    }, // ref. https://storybook.js.org/blog/integrate-nextjs-and-storybook-automatically/
     msw: {
       handlers: [
         rest.get(urlPathForBooksByKeywordsAndStartIndex, (req, res, ctx) => {
@@ -43,10 +29,26 @@ export const Default: Story = {
   },
 };
 
+export default meta;
+type Story = StoryObj<typeof SearchPage>;
+
 export const EmptySearchQuery: Story = {
   parameters: {
     nextjs: {
       appDirectory: true,
     },
+  },
+};
+
+export const SantaClausSearch: Story = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        query: {
+          keywords: "santa+claus",
+        },
+      },
+    }, // ref. https://storybook.js.org/blog/integrate-nextjs-and-storybook-automatically/
   },
 };
