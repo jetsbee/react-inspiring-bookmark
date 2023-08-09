c7a98a10ab9a8b4b4e21f09d90edb16def5ca034
Jaeseob Hwang <jetsbee@gmail.com>
2023-08-09 18:35:22 +0900

    Add bookmark screen storybook

---
+++ apps/storybook-react/src/stories/pages/Bookmarks.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { GlobalLayout } from "app/src/components/GlobalLayout";
+import BookmarksScreen from "app/src/pages/Bookmarks";
+import { Providers } from "app/src/providers";
+
+const meta: Meta<typeof BookmarksScreen> = {
+  title: "BookmarksScreen",
+  component: BookmarksScreen,
+  tags: ["autodocs"],
+  decorators: [
+    (story) => (
+      <Providers>
+        <GlobalLayout>{story()}</GlobalLayout>
+      </Providers>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof BookmarksScreen>;
+
+export const Default: Story = {};
