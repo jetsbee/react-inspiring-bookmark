087f4131d86a2c0b42b7d573d1ce98af5cb35efb
Jaeseob Hwang <jetsbee@gmail.com>
2023-08-09 17:57:28 +0900

    Clean font setup

--- packages/app/src/tamagui.config.ts
+++ packages/app/src/tamagui.config.ts
@@ -1,5 +1,5 @@
 import { createAnimations } from "@tamagui/animations-react-native";
-import { createInterFont } from "@tamagui/font-inter";
+import { config as baseConfig } from "@tamagui/config";
 import { createMedia } from "@tamagui/react-native-media-driver";
 import { shorthands } from "@tamagui/shorthands";
 import { themes, tokens } from "@tamagui/themes";
@@ -25,60 +25,14 @@ const animations = createAnimations({
   },
 });
 
-const headingFont = createInterFont({
-  size: {
-    6: 15,
-  },
-  transform: {
-    6: "uppercase",
-    7: "none",
-  },
-  weight: {
-    6: "400",
-    7: "700",
-  },
-  color: {
-    6: "$colorFocus",
-    7: "$color",
-  },
-  letterSpacing: {
-    5: 2,
-    6: 1,
-    7: 0,
-    8: -1,
-    9: -2,
-    10: -3,
-    12: -4,
-    14: -5,
-    15: -6,
-  },
-  face: {
-    700: { normal: "InterBold" },
-  },
-});
-
-const bodyFont = createInterFont(
-  {
-    face: {
-      700: { normal: "InterBold" },
-    },
-  },
-  {
-    sizeSize: (size) => Math.round(size * 1.1),
-    sizeLineHeight: (size) => Math.round(size * 1.1 + (size > 20 ? 10 : 10)),
-  },
-);
-
 export const config: any = createTamagui({
+  ...baseConfig,
   defaultFont: "body",
   animations,
   shouldAddPrefersColorThemes: true,
   themeClassNameOnRoot: true,
   shorthands,
-  fonts: {
-    body: bodyFont,
-    heading: headingFont,
-  },
+  fonts: baseConfig.fonts,
   themes,
   tokens,
   media: createMedia({
