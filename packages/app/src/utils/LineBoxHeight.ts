1b48008b3f525ebbb86ba563c1ee8ee4d7ab13d1
Jaeseob Hwang <jetsbee@gmail.com>
2023-08-12 12:23:03 +0900

    Fix types

--- packages/app/src/utils/LineBoxHeight.ts
+++ packages/app/src/utils/LineBoxHeight.ts
@@ -1,6 +1,6 @@
 // ref. {ProjectDir}/node_modules/@tamagui/config/src/fonts.ts
-export const headingLineHeightSize = (size) =>
+export const headingLineHeightSize = (size: number) =>
   Math.round(size * 1.1 + (size < 30 ? 10 : 5));
 
-export const bodyLineHeightSize = (size) =>
+export const bodyLineHeightSize = (size: number) =>
   Math.round(size * 1.1 + (size >= 12 ? 8 : 4));
