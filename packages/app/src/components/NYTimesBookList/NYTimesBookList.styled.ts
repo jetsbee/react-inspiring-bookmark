9f3f77e95c188774bdf82390826826fc60480c63
Jaeseob Hwang <jetsbee@gmail.com>
2023-08-18 15:49:00 +0900

    Style heading for NYTimes book list

--- packages/app/src/components/NYTimesBookList/NYTimesBookList.styled.ts
+++ packages/app/src/components/NYTimesBookList/NYTimesBookList.styled.ts
@@ -6,6 +6,12 @@ import { StyledBaseH1 } from "../StyledBase";
 
 export const StyledH1 = styled(StyledBaseH1, {
   fontWeight: "300",
+  $xxs: {
+    fontSize: 24,
+    get lineHeight() {
+      return headingLineHeightSize(this.fontSize);
+    },
+  },
   fontSize: 28,
   get lineHeight() {
     return headingLineHeightSize(this.fontSize);
